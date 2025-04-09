a6ddaf3751925e2239df3a77299c3309dc46c0e8
zeotoni <zeotoni666@gmail.com>
2025-04-09 20:57:30 -0300

    feat: adds book component && css variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BookComponent } from './shared/components/book/book.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
